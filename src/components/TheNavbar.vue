89f0d614276e90cd96c998e5d98bfce0e5440d06
ahmed-kaif <kaifkhan.rcs@gmail.com>
2021-04-19 07:53:08 +0600

    added nav component

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        Rownok Asif
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="grey">
+        About
+      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer
+      v-model="drawer"
+      temporary
+      absolute
+      bottom
+      class="indigo"
+    >
+      <p>Test</p>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    }
+  },
+}
+</script>
+
+<style scoped></style>
